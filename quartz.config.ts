1878f56f71de5001774e2e5b850dd11c14f6f95c
xuzenghang <xuzenghang@gmail.com>
2024-11-03 15:00:58 +0800

    Quartz sync: Nov 3, 2024, 3:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 XZH's blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
